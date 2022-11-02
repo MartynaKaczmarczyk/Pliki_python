1b63305310b9f67b495f5c80674bfe0b0499f970
MartynaKaczmarczyk <116656874+MartynaKaczmarczyk@users.noreply.github.com>
2022-11-02 21:10:27 +0100

    Delete plik.py

--- plik.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Wpisz swoją liczbę całkowitą dodatnią: "))
-g = "*"
-spacja = " "
-for i in range(n):
-    print(spacja * (n - i) + g * i + "*" + g * i + spacja * (n - 1))
-    i += 1
-
-print(spacja * (n - 1) + "|_|")"""

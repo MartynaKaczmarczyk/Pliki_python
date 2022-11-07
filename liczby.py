c978dd35269fdd19645d187cbf62b9dc0fa86198
MartynaKaczmarczyk <116656874+MartynaKaczmarczyk@users.noreply.github.com>
2022-11-07 15:55:24 +0100

    Update liczby.py

--- liczby.py
+++ liczby.py
@@ -1,7 +1,2 @@
-n = int(input("Wpisz liczbę pobieranych liczb: "))
-suma = 0
-for i in range(n):
-    m = float(input("Twoja liczba: "))
-    suma = suma + m
-sr = suma / n
-print(sr)
+suma = 2 + 5
+print(suma)

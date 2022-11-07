8499b86b0c7f54f70ee8de0a96fb0909e6d141ef
MartynaKaczmarczyk <116656874+MartynaKaczmarczyk@users.noreply.github.com>
2022-11-07 20:18:22 +0100

    Update liczby.py

--- liczby.py
+++ liczby.py
@@ -1,25 +1,3 @@
-import unittest
-
-class LearnTest(unittest.TestCase):
-  def test(self):
-    pass
-
-
-suma = 2 + 5
-print(suma)
-
-on: 
-  push:
-    branches: [master]
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v2
-      - uses: actions/setup-python@v2
-        with:
-          python-version: "3.x"
-      - name: install
-        run: |
-          pip install -U pytest
-      - run: python3 -m pytest tests.py
+def suma(x, y):
+  suma = x + y
+  return suma

f7eec604a55f63317300b511010db6b4b1f2a84d
MartynaKaczmarczyk <116656874+MartynaKaczmarczyk@users.noreply.github.com>
2022-11-07 15:56:14 +0100

    Update liczby.py

--- liczby.py
+++ liczby.py
@@ -1,2 +1,18 @@
+on: 
+  push:
+    branches: [master]
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: actions/setup-python@v2
+        with:
+          python-version: "3.x"
+      - name: install
+        run: |
+          pip install -U pytest
+      - run: python3 -m pytest tests.py
+
 suma = 2 + 5
 print(suma)
